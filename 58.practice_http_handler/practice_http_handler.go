7eed1caaf8d80c8f164282686181a3022797eb00
sskim <sskim@gameon.co.jp>
2016-07-26 11:50:22 +0900

    complete tour-58

---
+++ 58.practice_http_handler/practice_http_handler.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+type String string
+
+type Struct struct {
+  Greeting string
+  Punct string
+  Who string
+}
+
+func (h String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, h)
+}
+
+func (h *Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "%s %s %s", h.Who, h.Punct, h.Greeting)
+}
+
+func main() {
+  http.Handle("/string", String("I'm a frayed knot."))
+  http.Handle("/struct", &Struct {"Hello", ":", "Gophers!"})
+
+  http.ListenAndServe("localhost:4000", nil)
+}
